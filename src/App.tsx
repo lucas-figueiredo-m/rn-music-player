9aad6c7a58699d3199a9d6965a2e0c598b2f0b7a
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2021-09-29 00:29:50 -0300

    Removes spotify oauth authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { StatusBar } from 'react-native'
 import { ApolloProvider } from "@apollo/client";
 import { GraphqlClient } from 'services/GraphqlClient';
 import Router from 'navigation';
 import { Colors } from 'styles';
-import AppContext from 'contexts/AppContext';
-import { UserData } from 'store/interfaces/SpotifyInterfaces';
-import { AuthorizeResult } from 'react-native-app-auth';
-
 
 const App: React.FC = () => {
-  const [user, setUser] = useState<UserData | null>(null)
-  const [auth, setAuth] = useState<AuthorizeResult | null>(null)
   
   return (
     <ApolloProvider client={GraphqlClient}>
-      <AppContext.Provider value={{ user, setUser, auth, setAuth }}>
-        <StatusBar translucent backgroundColor={Colors.TRANSPARENT} barStyle='light-content' />
-        <Router />
-      </AppContext.Provider>
+      <StatusBar translucent backgroundColor={Colors.TRANSPARENT} barStyle='light-content' />
+      <Router />
     </ApolloProvider>
   )
 };
